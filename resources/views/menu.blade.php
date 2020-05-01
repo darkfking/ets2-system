d91222bdb62c4fda83fb705880001c8bd6f2ea14
Kuba <kubaj26@gmail.com>
2020-05-01 15:13:03 +0200

    Fix admin

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,6 +1,8 @@
 <div class="sidenav">
     <a href="{{route('home')}}">Start</a>
-    <a href="{{route('admin')}}">Administracja</a>
+    @if(Auth::user()->ranga == 'Admin')
+        <a href="{{route('admin')}}">Administracja</a>
+    @endif
     <a href="{{route('rozpiski.index')}}">Rozpiski</a>
     <a href="{{route('karta')}}">Karta kierowcy</a>
     <a href="{{route('cars.index')}}">Garaż</a>
